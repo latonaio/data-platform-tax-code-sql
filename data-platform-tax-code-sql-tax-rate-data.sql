6a835157a36af83f9e86f47ae607d724649b3a73
Yurino Oga <yurino.o@latona.io>
2022-10-30 22:32:43 +0900

    release

--- data-platform-tax-code-sql-tax-rate-data.sql
+++ data-platform-tax-code-sql-tax-rate-data.sql
@@ -2,9 +2,9 @@ CREATE TABLE `data_platform_tax_code_tax_rate_data`
 (
   `Country`              varchar(3) NOT NULL,
   `TaxCode`              varchar(2) NOT NULL,
-  `ValidityEndDate`      varchar(80) NOT NULL,
-  `ValidityStartDate`    varchar(80) DEFAULT NULL,
-  `TaxRate`              varchar(6) DEFAULT NULL,
+  `ValidityEndDate`      date NOT NULL,
+  `ValidityStartDate`    date DEFAULT NULL,
+  `TaxRate`              float(6) DEFAULT NULL,
     PRIMARY KEY (`Country`, `TaxCode`, `ValidityEndDate`),
     CONSTRAINT `DataPlatformTaxCodeTaxRateData_fk` FOREIGN KEY (`Country`, `TaxCode`) REFERENCES `data_platform_tax_code_tax_code_data` (`Country`, `TaxCode`)
 ) ENGINE = InnoDB
