ebc67322540b7ff88125c1ea5b7f3d27c95b27cd
Yurino Oga <yurino.o@latona.io>
2022-11-23 22:52:58 +0900

    release

--- data-platform-tax-code-sql-tax-code-data-setup.sql
+++ data-platform-tax-code-sql-tax-code-data-setup.sql
@@ -4,10 +4,10 @@ INSERT INTO `data_platform_tax_code_tax_code_data` (`Country`, `TaxCode`)
 VALUES
     ('JP', 'A0');
     ('JP', 'A1');
-    ('JP', 'A6');
     ('JP', 'A8');
+    ('JP', 'A9');
     ('JP', 'V0');
     ('JP', 'V1');
-    ('JP', 'V6');
     ('JP', 'V8');
+    ('JP', 'V9');
 UNLOCK TABLES;
--- data-platform-tax-code-sql-tax-code-text-data-setup.sql
+++ data-platform-tax-code-sql-tax-code-text-data-setup.sql
@@ -4,10 +4,10 @@ INSERT INTO `data_platform_tax_code_tax_code_text_data` (`Country`, `TaxCode`, `
 VALUES
     ('JP', 'A0', 'JA', '不課税売上');
     ('JP', 'A1', 'JA', '課税売上');
-    ('JP', 'A6', 'JA', '免税売上');
     ('JP', 'A8', 'JA', '非課税売上');
+    ('JP', 'A9', 'JA', '免税売上');
     ('JP', 'V0', 'JA', '不課税仕入');
     ('JP', 'V1', 'JA', '課税仕入');
-    ('JP', 'V6', 'JA', '免税仕入');
     ('JP', 'V8', 'JA', '非課税仕入');
+    ('JP', 'V9', 'JA', '免税仕入');
 UNLOCK TABLES;
