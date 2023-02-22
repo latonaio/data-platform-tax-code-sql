c717a8ce576f384f054b0181d1bf4c658ec6bd82
MiyuYokoyama0109 <miyu.y@latona.io>
2023-02-22 13:36:01 +0900

    release

--- data-platform-tax-code-sql-tax-code-data-setup.sql
+++ data-platform-tax-code-sql-tax-code-data-setup.sql
@@ -2,8 +2,8 @@ LOCK TABLES `data_platform_tax_code_tax_code_data` WRITE;
 
 INSERT INTO `data_platform_tax_code_tax_code_data` (`Country`, `TaxCode`)
 VALUES
-    ('JP', '0');
-    ('JP', '1');
-    ('JP', '8');
+    ('JP', '0'),
+    ('JP', '1'),
+    ('JP', '8'),
     ('JP', '9');
 UNLOCK TABLES;
--- data-platform-tax-code-sql-tax-code-text-data-setup.sql
+++ data-platform-tax-code-sql-tax-code-text-data-setup.sql
@@ -2,8 +2,8 @@ LOCK TABLES `data_platform_tax_code_tax_code_text_data` WRITE;
 
 INSERT INTO `data_platform_tax_code_tax_code_text_data` (`Country`, `TaxCode`, `Language`, `TaxCodeName`)
 VALUES
-    ('JP', '0', 'JA', '不課税');
-    ('JP', '1', 'JA', '課税');
-    ('JP', '8', 'JA', '非課税');
+    ('JP', '0', 'JA', '不課税'),
+    ('JP', '1', 'JA', '課税'),
+    ('JP', '8', 'JA', '非課税'),
     ('JP', '9', 'JA', '免税');
 UNLOCK TABLES;
